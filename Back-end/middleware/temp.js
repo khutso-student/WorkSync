6ae11c5021d9ed408039f1e1dcdd3b1d8bf5701a
khutso-student <khutsomakunyane1@gmail.com>
2025-07-16 12:26:45 +0200

    Rename AUTH.js to temp.js

---
+++ Back-end/middleware/temp.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+exports.protect = (req, res, next) => {
+    const auth = req.headers.authorization;
+    console.log("🛡️ Incoming token:", auth);
+
+    if(!auth || !auth.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "No token provided" });
+    }
+
+    const token = auth.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log('🔐 Decoded token:', decoded);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: "Invalid or expired token" });
+    }
+
+};
+
+exports.authorize = (...roles) => {
+    return ( req, res, next ) => {
+        if(!roles.includes(req.user.role)){
+            return res.status(403).json({ message: "Forbidden: Access denied" });
+        }
+        next();
+    };
+};
